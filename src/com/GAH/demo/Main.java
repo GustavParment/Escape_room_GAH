134c86aa51409c85cff8f34b51e846712bdb6842
GustavParment <gustav.parment1993@gmail.com>
2023-10-17 12:46:36 +0200

    first commit

--- src/com/GAH/demo/Main.java
+++ src/com/GAH/demo/Main.java
@@ -4,27 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int a = 0;
-
-        int b = 1;
-
-        int sum = 0;
-
-        int result = 0;
-
-        for (int i = 0; sum < 2_000000; i++) {
-
-            sum = a +b ;
-            a = b;
-            b = sum;
-
-            if (sum % 2 == 0){
-
-                result += sum;
-            }
-
-        }
-        System.out.println(result);
 
 
     }
